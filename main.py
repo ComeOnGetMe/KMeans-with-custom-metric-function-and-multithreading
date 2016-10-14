c25ad25b5dbf0abbbcbfabdc9847234e5f9eba51
ComeOnGetMe <yzp451312410@gmail.com>
2016-10-14 12:21:21 -0700

    reorganize

--- main.py
+++ main.py
@@ -1,22 +1,3 @@
-'''
-Accuracies of different prototype selection and metrics
-random
-    euclidean
-        k=5000 0.95
-kmeans
-    euclidean:
-        k=1 0.82
-        k=100 
-        k=1000 0.957
-        k=5000 0.9669
-        k=10000 0.9693
-    twosided tangent:
-        k=10 0.8439
-        k=100 0.9667
-    onesided tangent: 
-        k=100 0.9426
-'''
-
 import numpy as np
 from PIL import Image
 from numpy import linalg as LA
@@ -24,38 +5,35 @@ from sklearn.datasets import fetch_mldata
 from kmeans import kmeans
 
 mnist = fetch_mldata('MNIST original', data_home='./data')
+train_dataset = mnist.data[:60000]
+train_labels = mnist.target[:60000]
+test_dataset = mnist.data[-10000:]
+test_labels = mnist.target[-10000:]
 
-def imageshow(img, name='', showflag=True):
-    im = Image.fromarray(np.reshape(img,(28,28)).astype(np.uint8))
-    if showflag:
-        im.show()
-    if name!='':
-        im.save(name)
-    return im
-
-def score(test_dataset, test_labels, prototypes, prototype_labels, metric):
+def score(test_dataset, test_labels, prototypes, prototype_labels, pmetric):
     res = np.empty((len(test_dataset)), dtype=np.uint8)
-    D = metric(test_dataset, prototypes)
-    res = np.argmin(D, axis=1)
-    s=np.mean(res==test_labels)
-    return res,s 
+    D = pmetric(test_dataset, prototypes)
+    res = D.argmin(axis=1)
+    s=np.mean(prototype_labels[res]==test_labels)
+    return res,s
 
 # TD
 from mpdist import mpdist
 from kmeans import kmeans
+from tangentDistance import *
 
-k=10
+k=100
 prototypes=[]
 prototype_labels=[]
 m1TD = lambda X,Y: mpdist(X, Y, oneSideTD, ("tangentDistance","ctypes","numpy"))
 for i in xrange(10):
     print 'clustering %d-th label' %(i)
-    train_dataset = mnist.data[mnist.target==i]
-    test_dataset = mnist.target[mnist.target==i]
-    init_center = np.array(train_dataset[:(k/10)])
-    c,cid,dist=kmeans(train_dataset, init_center, metric=m1TD, pdist=True)
+    cur_train_dataset = mnist.data[mnist.target==i]
+    cur_test_dataset = mnist.target[mnist.target==i]
+    init_center = np.array(cur_train_dataset[:(k/10)])
+    c,cid,dist=kmeans(cur_train_dataset, init_center, metric=m1TD, pdist=True)
     for cc in c: prototypes.append(cc)
     prototype_labels += [i]*(k/10)
 
-cls, s=score(mnist.data[-10000:], mnist.target[-10000:], prototypes, prototype_labels, m1TD)
+cls, s=score(test_dataset, test_labels, prototypes, np.array(prototype_labels), m1TD)
 print s
